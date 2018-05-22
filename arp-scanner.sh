3cffc34bdfcee2d41c70c63d237fe240e67e43f6
Cypher <crypto-cypher@users.noreply.github.com>
2018-05-22 16:38:42 -0400

    Update arp-scanner.sh

--- arp-scanner.sh
+++ arp-scanner.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 # Variables
-wireless_int="wlp2s0"
-wired_int="wlp2s0"
+wireless_int="enp0s31f6"
+wired_int="enp0s31f6"
 wireless_ip="$(ifconfig $wireless_int 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://')"
 
 # Check if IP is assigned to variable interface
 if [[ $wireless_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         echo "Interface received IP address: $wireless_ip, DHCP is probably working"
 else
-        echo "Interface has no IP address, DHCP is probably not working"
+      	echo "Interface has no IP address, DHCP is probably not working"
 
-        # Create log file
-        rm slog.txt;touch slog.txt
+	# Create log file
+       	sudo rm slog.txt;touch slog.txt
 
-        bandwidth="100000"
+       	bandwidth="100000"
 
-        # Common network ranges
-        sudo arp-scan -g -B $bandwidth --interface=$wired_int 192.168.0.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
-        sudo arp-scan -g -B $bandwidth --interface=$wired_int 192.168.1.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
-        sudo arp-scan -g -B $bandwidth --interface=$wired_int 192.168.2.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
-        sudo arp-scan -g -B $bandwidth --interface=$wired_int 172.16.0.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
-        sudo arp-scan -g -B $bandwidth --interface=$wired_int 172.16.1.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
-        sudo arp-scan -g -B $bandwidth --interface=$wired_int 172.16.2.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
-        sudo arp-scan -g -B $bandwidth --interface=wlp2s0 10.0.0.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
-        sudo arp-scan -g -B $bandwidth --interface=$wired_int 10.0.1.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
-        sudo arp-scan -g -B $bandwidth --interface=$wired_int 10.0.2.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	# Common network ranges
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 192.168.0.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 192.168.1.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 192.168.2.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 172.16.0.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 172.16.1.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 172.16.2.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 10.0.0.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 10.0.1.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+       	sudo arp-scan --arpspa 192.168.50.5 -g -B $bandwidth --interface=$wired_int 10.0.2.0/24 |grep -P '^\d+(\.\d+){3}\s'|tee -a slog.txt
+
+	# Set static IP
+	sudo ifconfig $wired_int $(head -1 slog.txt | cut -d"." -f1,2,3).250/24 up
 fi
